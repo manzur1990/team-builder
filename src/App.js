68f5ad7dedcf65c028aac479a25b98a0382700da
JM <manzur-1990@hotmail.com>
2020-04-21 20:45:57 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Form from './Form';
+import MemberList from "./MemberList"
+
 import './App.css';
 
 function App() {
+
+  const [avenger, setAvenger] = useState([
+    {
+      name: "Tony Stark",
+      heroName: "Iron Man",
+      superPower: "Technology"
+    }]);
+    
+    const addNewAvenger = data => {
+      console.log("addNewAvenger:", data)
+      const newAvenger = {
+        name: data.name,
+        heroName: data.heroName,
+        superPower: data.superPower
+      };
+
+    setAvenger([...avenger, newAvenger]);
+  };
+
+  console.log("from app:",avenger)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Form addNewAvenger={addNewAvenger}/>
+      <MemberList avenger={avenger}/>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/MemberList.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const MemberList = (props) => {
+    console.log("avanger:", props)
+
+
+    return (
+        <div>
+            {
+                props.avenger.map(name => (
+                    <div key={name.id}>
+                        <h2>{name.name}</h2>
+                        <p>{name.heroName}</p>
+                        <p>{name.superPower}</p>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+
+
+export default MemberList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+
+// const MemberList = (props) => {
+
+//     return (
+//         <div>
+
+//             {
+
+//                 props.infoMember.map(name => (
+//                     <div key={name.id}>
+//                         <h2>{name.name}</h2>
+//                         <p>{name.heroName}</p>
+//                         <p>{name.superPower}</p>
+//                         <p>{name.job}</p>
+//                     </div>
+
+//                 ))
+
+//             }
+
+
+//         </div>
+//     )
+
+
+
+// }
+
+
+// export default MemberList;
